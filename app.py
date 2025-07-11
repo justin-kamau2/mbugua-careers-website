e44af3368a6f1e6afa3875ec05db354ea516e9fa
justin-kamau2 <149835965+justin-kamau2@users.noreply.github.com>
2025-07-11 11:29:41 +0000

    changes

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ JOBS = [
     'id':1,
     'title': 'Data Analyst',
     'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
+    'salary': 'Rs. 100,000'
   },
   {
     'id':2,
     'title': 'Data Scientist',
     'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
+    'salary': 'Rs. 150,000'
   },
   {
     'id':3,
     'title': 'Frontend Engineer',
     'location': 'Remote',
-    'salary': 'Rs. 12,00,000'
+    'salary': 'Rs. 120,000'
   },
   {
     'id':4,
